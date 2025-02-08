5728f972fb1a64d23aa73dee02526aa9810474b3
Jingren Li <lij9@tcd.ie>
2025-02-08 18:21:25 +0000

    initial commit for homepage and nav bar

---
+++ frontend/src/pages/homePage/homePage.style.ts
@@ -0,0 +1,12 @@
+'use client'
+import { makeStyles } from "@griffel/react"
+
+export const useStyle = makeStyles({
+    header:{
+        display: "flex",
+        flexDirection: "column",
+        background: "#008bbf",
+        color: "white",
+        padding: "10px 30px"
+    }
+})
